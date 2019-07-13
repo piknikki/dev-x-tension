6e29aa31ad9941bda61c91086988d421969157f1
Nikki Petersen <piknik@me.com>
2019-07-13 08:31:15 -0600

    repairing dependencies

---
+++ client/build/precache-manifest.7bca8a66f507bdd26b7079b29ffa7073.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5418cbd79eca064a11db",
+    "url": "/static/css/main.e150ea9c.chunk.css"
+  },
+  {
+    "revision": "5418cbd79eca064a11db",
+    "url": "/static/js/main.e04bd628.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2694542b9b4b70f16c8c",
+    "url": "/static/css/2.7a149ea4.chunk.css"
+  },
+  {
+    "revision": "2694542b9b4b70f16c8c",
+    "url": "/static/js/2.1da07138.chunk.js"
+  },
+  {
+    "revision": "9506ec8cd3fee2d7fe45efebba06f1c3",
+    "url": "/index.html"
+  }
+];
